e699d5cf2bad1d46808e6270df4ffb68f9989b33
tekiann <38327786+tekiann@users.noreply.github.com>
2022-02-15 22:18:56 -0300

    Exercicio 2

---
+++ Exercicio2/Exercicio2/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Exercicio2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtNascimento = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.response = new System.Windows.Forms.Label();
+            this.submit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dtNascimento
+            // 
+            this.dtNascimento.Location = new System.Drawing.Point(58, 32);
+            this.dtNascimento.Name = "dtNascimento";
+            this.dtNascimento.Size = new System.Drawing.Size(200, 20);
+            this.dtNascimento.TabIndex = 0;
+            this.dtNascimento.ValueChanged += new System.EventHandler(this.DateTimePicker1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(54, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Sua Data de Nascimento";
+            // 
+            // response
+            // 
+            this.response.AutoSize = true;
+            this.response.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.response.ForeColor = System.Drawing.Color.White;
+            this.response.Location = new System.Drawing.Point(58, 67);
+            this.response.Name = "response";
+            this.response.Size = new System.Drawing.Size(86, 20);
+            this.response.TabIndex = 2;
+            this.response.Text = "Resultado:";
+            // 
+            // submit
+            // 
+            this.submit.Location = new System.Drawing.Point(62, 102);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(196, 23);
+            this.submit.TabIndex = 3;
+            this.submit.Text = "Submit";
+            this.submit.UseVisualStyleBackColor = true;
+            this.submit.Click += new System.EventHandler(this.Submit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(311, 514);
+            this.Controls.Add(this.submit);
+            this.Controls.Add(this.response);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtNascimento);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dtNascimento;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label response;
+        private System.Windows.Forms.Button submit;
+    }
+}
+
---
+++ Exercicio2/Exercicio2/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercicio2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void DateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Submit_Click(object sender, EventArgs e)
+        {
+            DateTime dateNascimento = new DateTime();
+            dateNascimento = Convert.ToDateTime(dtNascimento.Text);
+            if (dateNascimento.AddYears(18) < DateTime.Today)
+            {
+                response.Text = "Resultado: Pode possuir CNH";
+            }
+            else
+            {
+                response.Text = "Resultado: Não pode possuir CNH";
+            }
+        }
+    }
+}
