f8ce0dcf1f37fbd208cb5775e6ebbcecc1e3474a
Leonardo Ferreira <69694894+LFerr1@users.noreply.github.com>
2022-02-24 21:15:09 -0300

    Exercício Lista Usuário Concluído

--- ExercicioListaUsuario/ExercicioListaUsuario/Form2.cs
+++ ExercicioListaUsuario/ExercicioListaUsuario/Form2.cs
@@ -13,14 +13,12 @@ namespace ExercicioListaUsuario
     public partial class Form2 : Form
     {
         public List<Usuario> usuarios = new List<Usuario>();
-        public Form2(List<Usuario> usuarios)
+        public Form2(List<Usuario> cachedUsuarios)
         {
+            usuarios = cachedUsuarios;
             InitializeComponent();
-            comboBox1.SelectedText. = "Todos Usuários";
-            for (int i = 0; i < usuarios.Count; i++)
-            {
-                listBox1.Items.Add($"Usuario: {usuarios[i].Login}");
-            }
+            comboBox1.Text = "Todos Usuários";
+            
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -30,14 +28,14 @@ namespace ExercicioListaUsuario
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if(comboBox1.SelectedText == "Todos Usuários")
+            if(comboBox1.Text == "Todos Usuários")
             {
                 listBox1.Items.Clear();
                 for (int i = 0; i < usuarios.Count; i++)
                 {
                     listBox1.Items.Add($"Usuario: {usuarios[i].Login}");
                 }
-            }else if (comboBox1.SelectedText == "Usuários Inativos")
+            }else if (comboBox1.Text == "Usuários Inativos")
             {
                 listBox1.Items.Clear();
                 for (int i = 0; i < usuarios.Where(user => !user.Status).Count(); i++)
@@ -45,7 +43,7 @@ namespace ExercicioListaUsuario
                     listBox1.Items.Add($"Usuario: {usuarios.Where(user => !user.Status).ToList()[i].Login}");
                 }
             }
-            else if (comboBox1.SelectedText == "Usuários Ativos")
+            else if (comboBox1.Text == "Usuários Ativos")
             {
                 listBox1.Items.Clear();
                 for (int i = 0; i < usuarios.Where(user => user.Status).Count(); i++)
